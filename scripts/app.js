78cf7c9470e8c98f2b8576f18d37b87f45f0ec9b
LikaxJimin <hambardzumyanlika@gmail.com>
2022-02-17 15:31:33 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,24 @@ btn.addEventListener('mouseover', function(){
 let imageObject = document.querySelector('.cover-image img');
 })
 
+$(function(){
+
+    $('input[type="text"]').keyup(function(){
+        
+        var searchText = $(this).val().toUpperCase();
+        
+        $('.tv-search-result-ul-tm > li').each(function(){
+            
+            var currentLiText = $(this).text().toUpperCase(),
+                showCurrentLi = currentLiText.indexOf(searchText) !== -1;
+            //$(this).toggle( showCurrentLi);
+            if(showCurrentLi){
+              $(this).addClass('tv-in-tm').removeClass('tv-out-tm');
+            }else{
+              $(this).addClass('tv-out-tm').removeClass('tv-in-tm');
+            }
+            
+        });     
+    });
+
+});
