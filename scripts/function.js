eef45da5382d16f1cfb94f7ef3230dafbab84091
LikaxJimin <hambardzumyanlika@gmail.com>
2022-02-17 18:47:59 +0300

    Update function.js

--- scripts/function.js
+++ scripts/function.js
@@ -9,24 +9,24 @@
 // })
 
 
-$(function(){
+// $(function(){
  
-    $('input[type="text"]').keyup(function(){
+//     $('input[type="text"]').keyup(function(){
         
-        var searchText = $(this).val().toUpperCase();
+//         var searchText = $(this).val().toUpperCase();
         
-        $('.tv-search-result-ul-tm > li').each(function(){
+//         $('.tv-search-result-ul-tm > li').each(function(){
             
-            var currentLiText = $(this).text().toUpperCase(),
-                showCurrentLi = currentLiText.indexOf(searchText) !== -1;
-            // $(this).toggle( showCurrentLi);
-            if(showCurrentLi){
-              $(this).addClass('tv-in-tm').removeClass('tv-out-tm');
-            }else{
-              $(this).addClass('tv-out-tm').removeClass('tv-in-tm');
-            }
+//             var currentLiText = $(this).text().toUpperCase(),
+//                 showCurrentLi = currentLiText.indexOf(searchText) !== -1;
+//             // $(this).toggle( showCurrentLi);
+//             if(showCurrentLi){
+//               $(this).addClass('tv-in-tm').removeClass('tv-out-tm');
+//             }else{
+//               $(this).addClass('tv-out-tm').removeClass('tv-in-tm');
+//             }
             
-        });     
-    });
+//         });     
+//     });
  
-});
+// });
