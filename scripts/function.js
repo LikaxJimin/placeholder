736bfe89f4cebd0ef88eccd2a0b2c077a986d96c
LikaxJimin <hambardzumyanlika@gmail.com>
2022-02-24 23:03:01 +0300

    Update function.js

--- scripts/function.js
+++ scripts/function.js
@@ -30,3 +30,13 @@
 //     });
  
 // });
+document.querySelectorAll(".load-image").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const href = event.target.getAttribute("href");
+      const newImage = document.createElement("img");
+      event.preventDefault();
+      newImage.setAttribute("src", href);
+      document.body.insertBefore(newImage, event.target);
+      event.target.remove();
+    });
+  });
